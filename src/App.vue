eb50b1b0aeee2504a67fcdad4f280473572684b1
panc1028 <1437871473@qq.com>
2023-10-08 21:42:24 +0800

    提交消息

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="box">
+    {{ str }}
+  </div>
+</template>
+
+<script setup lang="ts">
+const str = 'www'
+</script>
+
+<style scoped lang="scss">
+.box {
+  height: 60px;
+}
+</style>
